af5f982c96f7cd9df8b46041600f7923faa5dd86
Daniel Duque <duqkefly@gmail.com>
2021-03-27 12:52:48 -0500

    pc --DD

--- js.js
+++ js.js
@@ -1,1 +1,16 @@
-js.js
+console.log('it works!');
+
+var myForm = document.querySelector("#myForm");
+var resultImage = document.querySelector(".resultImage");
+
+myForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    var formData = new FormData(e.target);
+    url = URL.createObjectURL(formData.get('image'));
+    renderImage(url);
+})
+
+function renderImage(url){
+    console.log(url);
+    resultImage.setAttribute('src', url)
+}
